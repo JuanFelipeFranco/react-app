d20b013b99089ca5094bd6929fa3e1e06727fdb5
Juan Felipe Franco <juanfelipefrancorosas@gmail.com>
2024-07-05 11:05:19 -0500

    [Crud App]-Agregando lista de productos

---
+++ src/services/ProductService.js
@@ -0,0 +1,17 @@
+
+const initProducts = [
+    {
+        name:'Monitor Samsung',
+        price:500,
+        description:'El monitor es increible'
+    },
+    {
+        name:'Iphon 15',
+        price:800,
+        description:'Telefono bueno'
+    }
+];
+
+export const listProduct= () => {
+    return initProducts
+}
