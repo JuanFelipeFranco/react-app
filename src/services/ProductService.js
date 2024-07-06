719b697df631617e9c25a81cf602202bf56a3ddf
Juan Felipe Franco <juanfelipefrancorosas@gmail.com>
2024-07-05 21:01:26 -0500

    [Crud App] - agregando id

--- src/services/ProductService.js
+++ src/services/ProductService.js
@@ -1,11 +1,13 @@
 
 const initProducts = [
     {
+        id:1,
         name:'Monitor Samsung',
         price:500,
         description:'El monitor es increible'
     },
     {
+        id:2,
         name:'Iphon 15',
         price:800,
         description:'Telefono bueno'
